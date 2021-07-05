7f33ae452d169826c860e1c9c7ea06ed83021160
Hugo del Solar <hudrec11@gmail.com>
2021-07-05 05:52:12 -0500

    refactor

--- poke_challenge/poke_challenge/settings.py
+++ poke_challenge/poke_challenge/settings.py
@@ -23,7 +23,7 @@ env = environ.Env()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fg3j++8_hb!##=t4$44six^qc)qwh9!4a_*nz&ooe6z!2ib)b@'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
